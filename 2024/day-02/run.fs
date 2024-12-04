ec35fddc37ea0da1903c0f0c0ad2154d33d61c33
Aquilla Sherrock <acsherrock@gmail.com>
2024-12-04 15:07:52 -0700

    finish day 2

--- 2024/day-02/run.fs
+++ 2024/day-02/run.fs
@@ -1,2 +1,29 @@
-﻿// For more information see https://aka.ms/fsharp-console-apps
-printfn "Hello from F#"
+﻿open System
+    
+let andf f g x =
+    f x && g x
+    
+let signOf n = n / abs n
+let subTup (a, b) = a - b
+
+let isSafe xs =
+    Seq.pairwise xs
+    |> Seq.map subTup
+    |> Seq.toList
+    |> function
+        | a::rest when a <> 0 && abs a < 4  ->
+            Seq.forall ((*) (signOf a) >> andf ((<) 0) ((>) 4)) rest
+        | _ -> false
+    
+let isSafeDamp xs = function
+    | _ when isSafe xs -> true
+    | _ ->
+       xs
+       |> Seq.mapi (fun i _ -> i)
+       |> Seq.exists (fun i -> xs |> Seq.removeAt i |> isSafe)
+
+    
+System.IO.File.ReadLines("input/input.txt")
+|> Seq.map (_.Split(" ") >> Seq.map int)
+|> Seq.sumBy (isSafeDamp >> Convert.ToInt16)
+|> printfn "%i"
