f34902a14b17ab163043fc825bef1c11ee9066df
Aquilla Sherrock <acsherrock@gmail.com>
2024-12-03 23:19:19 -0700

    add more days and fix projects

--- 2024/day-01/f/run.fs
+++ 2024/day-01/f/run.fs
@@ -1,12 +1,31 @@
 let split (delim: string) (str: string)=
     str.Split(delim)
 
-let run =
-    System.IO.File.ReadLines("./2024/day-01/f/input/part1.txt")
-    |> Seq.map (split "   " >> fun i -> int i[0], int i[1])
+let parseIntCols delim lines =
+    lines
+    |> Seq.map (split delim >> fun i -> int i[0], int i[1])
     |> Seq.toList
     |> List.unzip
-    |> fun (l, r) -> (List.sort l, List.sort r)
+
+let p1 input =
+    input
+    |> parseIntCols "   "
+    |> fun (l, r) -> List.sort l, List.sort r
     ||> List.zip
     |> List.sumBy (fun (l, r) -> abs(l - r))
-    |> printfn "%i"
+
+let countWhere pred xs =
+    List.filter pred xs |> List.length
+    
+let countWhereEq value =
+    countWhere (fun x -> x = value)
+    
+let p2Inefficient input =
+    let left, right = input |> parseIntCols "   "
+    left |> List.sumBy (fun x -> x * (countWhereEq x right))
+
+let input =
+    System.IO.File.ReadLines("input/part1.txt")
+
+printfn $"%i{p1 input}"
+printfn $"%i{p2Inefficient input}"
