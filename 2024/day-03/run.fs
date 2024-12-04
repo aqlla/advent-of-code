2e03c142c6ca13fe8156ec71024e83ac67c1a455
Aquilla Sherrock <acsherrock@gmail.com>
2024-12-04 15:58:39 -0700

    finish day 3 p1 2

--- 2024/day-03/run.fs
+++ 2024/day-03/run.fs
@@ -3,11 +3,9 @@ open System.Text.RegularExpressions
 
 let matches pattern str =
     Regex.Matches(str, pattern).Cast<Match>().Select(_.Value)
-    
-let ints = Seq.map int
 
 System.IO.File.ReadLines("input/input.txt")
 |> Seq.collect (matches "mul\(\d+,\d+\)")
-|> Seq.map (matches "\d+" >> ints >> Seq.fold (*) 1)
+|> Seq.map (matches "\d+" >> Seq.map int >> Seq.fold (*) 1)
 |> Seq.sum
 |> printfn "%d"
